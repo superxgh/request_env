8874d5fe94c1369d313593ca8039888e81546578
superxgh <mrsiwakorn@gmail.com>
2023-11-09 07:59:39 +0700

    Updte node script

--- init_docker.bat
+++ init_docker.bat
@@ -1,5 +1,12 @@
 @REM docker network rm req_network
 
+docker stop req_redis
+docker stop req_postgres
+docker stop req_nginx
+docker stop req_cloudflared
+
+docker rm req_redis
+docker rm req_postgres
 docker rm req_nginx
 docker rm req_cloudflared
 
