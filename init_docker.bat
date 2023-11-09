1d277593d21fc279d958a2646e253317dd0f8dca
superxgh <mrsiwakorn@gmail.com>
2023-11-09 07:03:04 +0700

    update docker compose add host network

--- init_docker.bat
+++ init_docker.bat
@@ -1,7 +1,8 @@
-@REM docker network create   --driver=bridge   --subnet=172.28.0.0/16  --ip-range=172.28.5.0/24   --gateway=172.28.5.254   req_network
+@REM docker network rm req_network
 
 docker rm req_nginx
 docker rm req_cloudflared
 
+@REM docker network create   --driver=bridge   --subnet=172.28.0.0/16  --gateway=172.28.0.1   req_network
 
 docker compose up -d
